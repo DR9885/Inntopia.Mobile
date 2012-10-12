5ba93ea749ab325a8bb1a9aa26c3ee74dccd428e
Daniel P. Rossi <DR9885@gmail.com>
2012-10-11 20:40:34 -0400

    inital commit

---
+++ Inntopia.BookingEngine.Mobile/Inntopia.BookingEngine.Mobile.Web/Controllers/SelectorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Inntopia.BookingEngine.Mobile.Web.Controllers
+{
+    public class SelectorController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public string Post(string value)
+        {
+            int i = 0;
+            return value;
+        }
+
+        // PUT api/<controller>/5
+        public string Put(int id, string value)
+        {
+            int i = 0;
+            return value;
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Inntopia.BookingEngine.Mobile/Inntopia.BookingEngine.Mobile.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Inntopia.BookingEngine.Mobile.Web.MvcApplication" Language="C#" %>
